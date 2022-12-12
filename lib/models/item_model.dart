5ba875d2eb9fd40a1771b7586dd1cb026a62b2ff
Karolina Eberst <karolinaeberst@gmail.com>
2022-12-12 20:44:01 +0100

    implement ItemModel

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel(
+      {required this.id,
+      required this.title,
+      required this.url,
+      required this.releaseDate});
+
+  final String id;
+  final String title;
+  final String url;
+  final DateTime releaseDate;
+}
